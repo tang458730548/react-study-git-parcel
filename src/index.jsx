a39d0db7f02da7c54dcb89f720eae1fc179918ad
LAPTOP-GN3LAP7B\admin <458730548@qq.com>
2019-07-29 17:57:30 +0800

    第一次提交

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Life from './pages/demo/Life'
+
+
+ReactDOM.render(
+    <Life></Life> ,
+    document.getElementById('root')
+)
---
+++ src/pages/demo/Life.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Child from './Child'
+import index from './index.less'
+
+export default class Life extends React.Component{
+
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         count:0
+    //     };
+    // }
+    
+    state = {
+        count:0
+    }
+
+    handleAdd=()=>{
+        // console.log(this.state)
+        this.setState({
+            count: this.state.count + 1
+        })
+        console.log("-----------------2")
+    }
+
+    handleClick(){
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    render (){  
+        let style = {
+            padding:50
+        }  
+        return <div className="content"> 
+            <p>React 生命周期</p>
+            <button onClick={this.handleAdd}>
+                点击一下
+            </button>
+            <button onClick={this.handleClick.bind(this)}>
+                点击一下
+            </button>
+            <p>{this.state.count}</p> 
+            <Child name={this.state.count} ></Child>
+        </div>
+    }
+}
